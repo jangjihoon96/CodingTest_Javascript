c04f3f64a09cd37992d9d1a3796348b3cdef1c16
Ji hoon <jangna96@gmail.com>
2023-09-28 00:00:47 +0900

    [unrated] Title: 조건 문자열, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,29 @@
+function solution(ineq, eq, n, m) {
+    var answer = 0;
+    if(ineq === '<' && eq === '='){
+        if(n <= m){
+            answer = 1
+        } else{
+            answer = 0;
+        }
+    } else if(ineq === '<' && eq === '!'){
+        if(n < m){
+            answer = 1
+        } else{
+            answer = 0;
+        }
+    } else if(ineq === '>' && eq === '='){
+        if(n >= m){
+            answer = 1
+        } else{
+            answer = 0;
+        }
+    } else if(ineq === '>' && eq === '!'){
+        if(n > m){
+            answer = 1
+        } else{
+            answer = 0;
+        }
+    }
+    return answer;
+}
