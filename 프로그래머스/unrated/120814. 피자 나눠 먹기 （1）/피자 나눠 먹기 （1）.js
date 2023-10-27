7fc9e1b26a9abe6ec0fdbd6f4359c226cc13b29d
Ji hoon <jangna96@gmail.com>
2023-10-27 19:56:40 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,10 @@
+const solution = (n) => {
+    // let result = 1;
+    // let pizza = 7;
+    // while(pizza < n){
+    //     result++
+    //     pizza = pizza*2
+    // }
+    // return result
+    return Math.ceil(n/7)
+}
